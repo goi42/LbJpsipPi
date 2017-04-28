ac459e1d37f27eb1e7f8e0453c529479180940fd
Michael Wilkinson <Michael Wilkinson michael.wilkinson@cern.ch>
2017-04-28 11:30:21 +0200

    changed B0MC GangaJob.py not to auto_resubmit and removed needless getDatasetMetadata() lines.

--- MC/B0JpsiKstar/GangaJob.py
+++ MC/B0JpsiKstar/GangaJob.py
@@ -20,16 +20,14 @@ j.outputfiles = [DiracFile('LimDVNtuples.root')] #has to match what's in optsfil
 # j.inputfiles = [LocalFile('/afs/cern.ch/user/m/mwilkins/luminosity/LUT-M1M2-L0PT.txt')] #inputting lookup table for the L0pt correction
 j.splitter = mySplitter
 # j.splitter.bulksubmit = False
-j.do_auto_resubmit = True 
+j.do_auto_resubmit = False
 filteredFiles = []
 path = "/MC/2012/Beam4000GeV-2012-MagUp-Nu2.5-Pythia8/Sim08f/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/11144001/ALLSTREAMS.DST"
 dataset = BKQuery(path).getDataset()
-datasetM = BKQuery(path).getDatasetMetadata()
 for f in dataset.files:
     filteredFiles.append(f)
 path = "/MC/2012/Beam4000GeV-2012-MagDown-Nu2.5-Pythia8/Sim08f/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/11144001/ALLSTREAMS.DST"
 dataset = BKQuery(path).getDataset()
-datasetM = BKQuery(path).getDatasetMetadata()
 for f in dataset.files:
     filteredFiles.append(f)
 j.inputdata = filteredFiles
